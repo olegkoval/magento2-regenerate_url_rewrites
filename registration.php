1e4bc1ae28cd388699e9229b8a45c6945b514f2e
mahmoud-ismail <40231537+mahmoud-ismail@users.noreply.github.com>
2019-02-26 16:44:07 +0300

    test pull command

--- registration.php
+++ registration.php
@@ -6,6 +6,7 @@
  * @author Oleg Koval <contact@olegkoval.com>
  * @copyright 2017 Oleg Koval
  * @license OSL-3.0, AFL-3.0
+ * new line to test pull command
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
